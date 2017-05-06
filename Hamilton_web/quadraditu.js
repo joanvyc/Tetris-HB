84618a77c9fad6cc5813ed81aeaa59e2eccb3ad7
johanitus1 <joan.vinyals@est.fib.upc.edu>
2017-05-06 23:23:34 +0200

    Changes to be committed: 	modified:   Hamilton_web/quadraditu.js

--- Hamilton_web/quadraditu.js
+++ Hamilton_web/quadraditu.js
@@ -17,14 +17,14 @@ function quadraditu(x, y) {
   // Post:draws the squeare.
   this.show = function(){
     switch(this.color){
-      case "default":fill(55, 55, 55);break;
-      case "cyan":   fill(109, 249, 244);break;
-      case "yellow": fill(242, 255, 0);  break;
-      case "purple": fill(166, 0, 255);  break;
-      case "green":  fill(0, 255, 19);   break;
-      case "red":    fill(255,0,0);      break;
-      case "blue":   fill(0,0,255);      break;
-      case "orange": fill(255, 153, 0);  break;
+      case -1:fill(55, 55, 55);break;
+      case 0:   fill(109, 249, 244);break;
+      case 1: fill(242, 255, 0);  break;
+      case 2: fill(166, 0, 255);  break;
+      case 3:  fill(0, 255, 19);   break;
+      case 4:    fill(255,0,0);      break;
+      case 5:   fill(0,0,255);      break;
+      case 6: fill(255, 153, 0);  break;
       default: break;
     }
     stroke(30,30,30);
