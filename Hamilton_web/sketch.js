a6aab24eb513a300f098e68c1ee18e2009a38183
teleportex <guillem.ramirez.miranda@est.fib.upc.edu>
2017-05-06 16:18:49 +0200

    Added basic structure of grid inp/out

--- Hamilton_web/sketch.js
+++ Hamilton_web/sketch.js
@@ -1,15 +1,27 @@
 var files = 20;
 var cols = 10;
-var grid[];
 var size = 10;
+
+var grid[];
 
 function setup() {
     var canvas;
+
     canvas = createCanvas(cols*size, files*size);
     canvas.parent('container');
     background(255, 255, 255);
+    for(var i = 0; i<200; ++i){
+        var quadraditu;
+        grid.push(quadraditu);
+
+    }
+
 }
 
 function draw() {
+    background(255);
+    for(var i = 0; i<200; ++i){
+        grid[i].show();
+    }
 
 }
