14403a070dab085f8032f4898f548309a3652289
johanitus1 <joan.vinyals@est.fib.upc.edu>
2017-05-06 15:54:28 +0200

    Changes to be committed: 	modified:   Hamilton_web/sketch.js

--- Hamilton_web/sketch.js
+++ Hamilton_web/sketch.js
@@ -1,11 +1,8 @@
-<<<<<<< HEAD
 var frameCounter = 0;
-=======
 var files = 20;
 var cols = 10;
 var grid[];
 var size = 10;
->>>>>>> cf4ab584bd7ea608b9f450aa09900fc75c4a9877
 
 function setup() {
     var canvas;
