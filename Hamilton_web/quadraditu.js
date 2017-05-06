a65ee26e6bf4ff52dec82cc7c3b028ea3f1ac7ce
johanitus1 <joan.vinyals@est.fib.upc.edu>
2017-05-06 23:25:37 +0200

    Changes to be committed: 	modified:   Hamilton_web/quadraditu.js

--- Hamilton_web/quadraditu.js
+++ Hamilton_web/quadraditu.js
@@ -5,7 +5,7 @@ function quadraditu(x, y) {
 
   // default  (empty)
   // <color>  (fill with this color)
-  this.color = "default";
+  this.color = -1;
 
   // default  (empty)
   // dynamics (falls when update)
@@ -17,14 +17,14 @@ function quadraditu(x, y) {
   // Post:draws the squeare.
   this.show = function(){
     switch(this.color){
-      case -1:fill(55, 55, 55);break;
-      case 0:   fill(109, 249, 244);break;
-      case 1: fill(242, 255, 0);  break;
-      case 2: fill(166, 0, 255);  break;
+      case -1: fill(55, 55, 55);break;
+      case 0:  fill(109, 249, 244);break;
+      case 1:  fill(242, 255, 0);  break;
+      case 2:  fill(166, 0, 255);  break;
       case 3:  fill(0, 255, 19);   break;
-      case 4:    fill(255,0,0);      break;
-      case 5:   fill(0,0,255);      break;
-      case 6: fill(255, 153, 0);  break;
+      case 4:  fill(255,0,0);      break;
+      case 5:  fill(0,0,255);      break;
+      case 6:  fill(255, 153, 0);  break;
       default: break;
     }
     stroke(30,30,30);
