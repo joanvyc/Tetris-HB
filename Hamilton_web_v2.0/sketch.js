78d9276363ab51653a6547faf695612799532c75
johanitus1 <joan.vinyals@est.fib.upc.edu>
2017-05-10 19:22:45 +0200

    Changes to be committed: 	modified:   Hamilton_web_v2.0/sketch.js

--- Hamilton_web_v2.0/sketch.js
+++ Hamilton_web_v2.0/sketch.js
@@ -19,32 +19,32 @@ var game_layout = function( p ) {
       update(p);
   };
 
-  p.keyPressed = function() {
-  	switch(p.keyCode){
-  		case p.LEFT_ARROW: 	dirH = "left";                              break;
-  		case p.RIGHT_ARROW:	dirH = "right";	                            break;
-  		case p.UP_ARROW: 		dirR = inverse_rotation ? "left" : "right";	break;
-  		case p.DOWN_ARROW: 	fallDelay = 5;			                        break;
-      case 67: {
-        pesa.make_static();
-        pesa.clean();
-        pesa = holder.hold(pesa);
-        pesa.origin.x = 0;
-        pesa.origin.y = -1;
-        pesa.draw();
-        break;
-      }
-  		case 32: fallDelay = 0;                                         break;
-  	}
-  }
-
-  p.keyReleased = function() {
-    switch(p.keyCode){
-  		case p.DOWN_ARROW:  fallDelay = 30;   break;
-      case p.LEFT_ARROW:  dirH = "stoped";  break;
-      case p.RIGHT_ARROW: dirH = "stoped";  break;
-  	}
-  };
-
 };
 var game = new p5(game_layout, 'tetris_matrix');
+
+var keyPressed = function() {
+  switch(keyCode){
+    case LEFT_ARROW: 	dirH = "left";                              break;
+    case RIGHT_ARROW:	dirH = "right";	                            break;
+    case UP_ARROW: 		dirR = inverse_rotation ? "left" : "right";	break;
+    case DOWN_ARROW: 	fallDelay = 5;			                        break;
+    case 67: {
+      pesa.make_static();
+      pesa.clean();
+      pesa = hold(pesa);
+      pesa.origin.x = 0;
+      pesa.origin.y = -1;
+      pesa.draw();
+      break;
+    }
+    case 32: fallDelay = 0;                                         break;
+  }
+}
+
+var keyReleased = function() {
+  switch(keyCode){
+    case DOWN_ARROW:  fallDelay = 30;   break;
+    case LEFT_ARROW:  dirH = "stoped";  break;
+    case RIGHT_ARROW: dirH = "stoped";  break;
+  }
+};
